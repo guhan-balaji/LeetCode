a8d30318d0f7e7529b6a07f0aa7647b6cb9530e0
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2022-11-15 03:04:32 +0000

    [LeetCode Sync] Runtime - 5 ms (17.49%), Memory - 2.6 MB (6.73%)

---
+++ my-folder/0205-isomorphic-strings/solution.rs
@@ -0,0 +1,13 @@
+impl Solution {
+    pub fn is_isomorphic(s: String, t: String) -> bool {
+        use std::collections::HashSet;
+
+        let set: HashSet<(char, char)> = s.chars().zip(t.chars()).collect();
+        let char_set_s: HashSet<char> = s.chars().collect();
+        let char_set_t: HashSet<char> = t.chars().collect();
+        
+        s.chars().count() == t.chars().count() && 
+        char_set_s.len() == char_set_t.len() && 
+        set.len() == char_set_s.len()
+    }
+}
