ec102b87c4bd378bb882eb0f388738dbe9a86e8b
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-06-03 21:21:50 +0000

    [LeetCode Sync] Runtime - 42 ms (86.74%), Memory - 17 MB (31.84%)

--- my-folder/0242-valid-anagram/solution.py
+++ my-folder/0242-valid-anagram/solution.py
@@ -1,16 +1,3 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        if len(s) != len(t):
-            return False
-        
-        for c in set(s):
-            if s.count(c) != t.count(c):
-                return False
-        return True
-        
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
