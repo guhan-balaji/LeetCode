3aea2af01df1f7f90f5b23e8c0a754e2407f94eb
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-06-03 19:22:35 +0000

    [LeetCode Sync] Runtime - 25 ms (99.46%), Memory - 12.3 MB (97.62%)

--- my-folder/0001-two-sum/solution.py
+++ my-folder/0001-two-sum/solution.py
@@ -1,16 +1,15 @@
 class Solution(object):
     def twoSum(self, nums, target):
-        nums_set = set(nums)
-        for (i, n) in enumerate(nums):
-            if (target - n) in nums_set:
-                print(i, n)
-                for (j, m) in enumerate(nums):
-                    if m == (target - n) and j != i:
-                        print(j, m)
-                        return [i, j]
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
         """
         
+        num_to_idx = {}
+
+        for idx, num in enumerate(nums):
+            if target - num in num_to_idx:
+                return [idx, num_to_idx[target - num]]
+            else:
+                num_to_idx[num] = idx
