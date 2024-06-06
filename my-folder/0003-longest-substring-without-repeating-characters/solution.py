dbd68f60805eee1225ddd4a7bd2df3215e36e09b
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-06-06 20:27:50 +0000

    [LeetCode Sync] Runtime - 57 ms (50.52%), Memory - 16.5 MB (98.97%)

--- my-folder/0003-longest-substring-without-repeating-characters/solution.py
+++ my-folder/0003-longest-substring-without-repeating-characters/solution.py
@@ -1,18 +1,24 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        if len(s) == 0:
-            return 0
-        else:
-            sub_string = dict({})
-            max_len = 1
-            l_idx = 0
 
-            for r_idx, c in enumerate(s):
-                sub_string[c] = sub_string[c] + 1 if c in sub_string else 1
-                while sub_string[c] > 1:
-                    sub_string[s[l_idx]] -= 1
-                    l_idx += 1
-                max_len = max(max_len, (r_idx - l_idx) + 1)
-            
-            return max_len
+        substring = set()
+        longest_length = length = 0
+        l = 0
+
+        for r, c in enumerate(s):
+            if c not in substring:
+                length +=1
+                longest_length = max(longest_length, length)
+                substring.add(c)
+            else:
+                while l <= r:
+                    if s[l] != c:
+                        substring.remove(s[l])
+                        l += 1
+                        length -= 1
+                    else:
+                        l += 1
+                        break
+        
+        return longest_length
         
