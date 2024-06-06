d1076caf9a544f38b515084bd1b4d6a598ca7e4b
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-06-06 20:09:44 +0000

    [LeetCode Sync] Runtime - 48 ms (83.79%), Memory - 16.5 MB (95.89%)

--- my-folder/0383-ransom-note/solution.py
+++ my-folder/0383-ransom-note/solution.py
@@ -1,6 +1,11 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        for c in set(ransomNote):
-            if ransomNote.count(c) > magazine.count(c):
+        m_counter = Counter(magazine)
+
+        for c in ransomNote:
+            if c not in m_counter or m_counter[c] < 1:
                 return False
+            else:
+                m_counter[c] -= 1
+            
         return True
