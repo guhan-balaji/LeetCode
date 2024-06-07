63926e365915f9eab0d848f20ff15b95cc8e36d0
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-06-07 21:28:14 +0000

    [LeetCode Sync] Runtime - 80 ms (24.95%), Memory - 19.9 MB (69.45%)

--- my-folder/0057-insert-interval/solution.py
+++ my-folder/0057-insert-interval/solution.py
@@ -1,20 +1,17 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        res = []
-        inserted = False
-        for [s, e] in intervals:
-            if e < newInterval[0]:
-                res.append([s, e])
-            elif s > newInterval[1]:
-                if not inserted:
-                    res.append(newInterval)
-                    inserted = True
-                res.append([s, e])
-            else:
-                newInterval[0] = min(s, newInterval[0]) 
-                newInterval[1] = max(e, newInterval[1]) 
-        
-        if not inserted:
-            res.append(newInterval)
+        final_intervals = []
 
-        return res
+        for [a, b] in intervals:
+            if b < newInterval[0]:
+                final_intervals.append([a, b])
+            elif a > newInterval[1]:
+                final_intervals.append(newInterval)
+                newInterval = [a, b]
+            else:
+                newInterval[0] = min(a, newInterval[0])
+                newInterval[1] = max(b, newInterval[1])
+
+        final_intervals.append(newInterval)
+
+        return final_intervals
