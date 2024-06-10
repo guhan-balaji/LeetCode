29396618f098a088db43b695212d3682d886057c
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-06-10 22:11:50 +0000

    [LeetCode Sync] Runtime - 37 ms (38.36%), Memory - 16.5 MB (72.48%)

--- my-folder/0070-climbing-stairs/solution.py
+++ my-folder/0070-climbing-stairs/solution.py
@@ -1,12 +1,30 @@
+from typing import Dict
 class Solution:
-    def climbStairs(self, n: int, memo = {}) -> int:
-        if n in memo:
-            return memo[n]
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        elif n == 2:
+            return 2
         else:
-            if n == 0:
-                return 1
-            elif n < 0:
-                return 0
-            else:
-                memo[n] = self.climbStairs(n - 1) + self.climbStairs(n - 2)
-                return memo[n]
+            ways = [0] * n
+            ways[0] = 1
+            ways[1] = 2
+
+            for i in range(2, n):
+                ways[i] = ways[i - 1] + ways[i - 2]
+
+            return ways[n - 1]
+    #     return self.rec(n, dict())
+
+    # def rec(self, n: int, memo: Dict[int, int] = dict()):
+    #     if n < 1:
+    #         return 0
+    #     elif n == 1:
+    #         return 1
+    #     elif n == 2:
+    #         return 2
+    #     elif n in memo:
+    #         return memo[n]
+    #     else:
+    #         memo[n] = self.rec(n - 1) + self.rec(n - 2)
+    #         return memo[n]
