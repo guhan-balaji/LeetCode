eaa347636060141ca9765d7e261c6cfe654d8397
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-06-11 20:41:51 +0000

    [LeetCode Sync] Runtime - 556 ms (94.11%), Memory - 23 MB (55.88%)

--- my-folder/1014-k-closest-points-to-origin/solution.py
+++ my-folder/1014-k-closest-points-to-origin/solution.py
@@ -1,3 +1,3 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-        return sorted(points, key = lambda p: math.sqrt(p[0] ** 2 + p[1] ** 2))[:k]
+        return sorted(points, key = lambda point: sqrt(point[0] **2 + point[1] ** 2))[:k]
