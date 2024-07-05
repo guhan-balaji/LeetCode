532a938bd2f080190bc8323dae84cb302081a827
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-05 01:28:31 +0000

    [LeetCode Sync] Runtime - 193 ms (42.49%), Memory - 18 MB (89.34%)

--- my-folder/0792-binary-search/solution.py
+++ my-folder/0792-binary-search/solution.py
@@ -1,18 +1,16 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        return self.bin_search(nums, target, 0, len(nums) - 1)
+        start, end = 0, len(nums) - 1
 
-    def bin_search(self, nums: List[int], target: int, start: int, end: int) -> int:
-        mid = start + (end - start) // 2
-        if start > end:
-            return -1
+        while start <= end:
+            mid = (start + end) // 2
 
-        if nums[mid] == target:
-            return mid
-        elif nums[mid] > target:
-            return self.bin_search(nums, target, start, mid - 1)
-        else:
-            return self.bin_search(nums, target, mid + 1, end)
-
-
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] > target:
+                end = mid - 1
+            else:
+                start = mid + 1
+        
+        return -1
         
