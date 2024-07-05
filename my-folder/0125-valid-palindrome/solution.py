402cb34476eca19ebb7c6baa8684a2e34c69ef5e
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-05 01:20:23 +0000

    [LeetCode Sync] Runtime - 55 ms (14.87%), Memory - 17.7 MB (29.64%)

--- my-folder/0125-valid-palindrome/solution.py
+++ my-folder/0125-valid-palindrome/solution.py
@@ -1,6 +1,4 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        s = [c for c in s.lower() if c.isalnum()]
-        return s == s[::-1] 
-
-        
+        s = list(filter(lambda c: c.isalnum(), s.lower()))
+        return s == s[::-1]
