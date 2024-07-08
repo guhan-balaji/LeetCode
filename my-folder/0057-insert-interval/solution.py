f0f08468de12c50db941de1cbad0c858a2fc7dd2
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-08 22:35:07 +0000

    [LeetCode Sync] Runtime - 82 ms (16.97%), Memory - 19.9 MB (53.72%)

--- my-folder/0057-insert-interval/solution.py
+++ my-folder/0057-insert-interval/solution.py
@@ -1,17 +1,17 @@
 class Solution:
     def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
-        final_intervals = []
-
-        for [a, b] in intervals:
-            if b < newInterval[0]:
-                final_intervals.append([a, b])
-            elif a > newInterval[1]:
-                final_intervals.append(newInterval)
-                newInterval = [a, b]
+        
+        merged = []
+        for idx, interval in enumerate(intervals):
+            if newInterval[0] > interval[1]:
+                merged.append(interval)
+            elif newInterval[1] < interval[0]:
+                interval, newInterval = newInterval, interval
+                merged.append(interval)
             else:
-                newInterval[0] = min(a, newInterval[0])
-                newInterval[1] = max(b, newInterval[1])
+                newInterval[0] = min(newInterval[0], interval[0])
+                newInterval[1] = max(newInterval[1], interval[1])
 
-        final_intervals.append(newInterval)
-
-        return final_intervals
+        merged.append(newInterval)
+        
+        return merged
