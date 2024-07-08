931865edcf2080dd97d8ca58139d61cda4f1ea31
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-08 22:52:59 +0000

    [LeetCode Sync] Runtime - 432 ms (67.06%), Memory - 18.8 MB (93.99%)

--- my-folder/0542-01-matrix/solution.py
+++ my-folder/0542-01-matrix/solution.py
@@ -1,27 +1,25 @@
 class Solution:
     def updateMatrix(self, mat: List[List[int]]) -> List[List[int]]:
-        rows = len(mat)
-        cols = len(mat[0])
-        dirs = [(0, -1), (0, 1), (1, 0), (-1, 0)]
         q = deque([])
 
-        for r in range(rows):
-            for c in range(cols):
+        for r in range(len(mat)):
+            for c in range(len(mat[0])):
                 if mat[r][c] == 0:
-                    q.append((r, c))
+                    q.append([r, c])
                 else:
                     mat[r][c] = -1
+        
 
         while q:
-            (r, c) = q.popleft()
-            for (dr, dc) in dirs:
-                nr, nc = r + dr, c + dc
-                if nr < 0 or nr >= rows or nc < 0 or nc >= cols or mat[nr][nc] != -1:
+            [r, c] = q.popleft()
+
+            for [dr, dc] in [[0, 1], [1, 0], [0, -1], [-1, 0]]:
+                x, y = r + dr, c + dc
+
+                if x < 0 or y < 0 or x >= len(mat) or y >= len(mat[0]) or mat[x][y] != -1:
                     continue
                 
-                mat[nr][nc] = mat[r][c] + 1
-                q.append((nr, nc))
+                mat[x][y] = mat[r][c] + 1
+                q.append([x, y])
         
         return mat
-                
-        
