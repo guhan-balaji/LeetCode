a50c89f9baebe484f67305cea9db54e553f72cab
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-12 22:36:46 +0000

    [LeetCode Sync] Runtime - 52 ms (47.12%), Memory - 16.5 MB (79.27%)

--- my-folder/1036-rotting-oranges/solution.py
+++ my-folder/1036-rotting-oranges/solution.py
@@ -1,44 +1,30 @@
-from typing import Tuple, Deque
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
-        q = deque([])
+        q: Deque[Tuple[int, int]] = deque([])
         fresh_oranges = 0
-        for x in range(len(grid)):
-            for y in range(len(grid[0])):
-                match grid[x][y]:
-                    case 1:
-                        fresh_oranges += 1
-                    case 2:
+
+        for r in range(len(grid)):
+            for c in range(len(grid[0])):
+                if grid[r][c] == 1:
+                    fresh_oranges += 1
+                elif grid[r][c] == 2:
+                    q.append((r, c))
+   
+        if fresh_oranges == 0 : return 0
+        if not q : return -1
+
+        minutes = 0
+        while q and fresh_oranges > 0:
+            for _ in range(len(q)):
+                r, c = q.popleft()
+                for dr, dc in [(1, 0), (0, 1), (-1, 0), (0, -1)]:
+                    x, y = r + dr, c + dc
+                    if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]) : continue
+                    if grid[x][y] == 1:
+                        grid[x][y] = 2
                         q.append((x, y))
-        
-        if fresh_oranges == 0:
-            return 0
-        elif not q:
-            return -1
-        else:
-            minutes = -1
-            while q:
-                q = self.bfs(q, grid)
-                minutes += 1
-                fresh_oranges -= len(q)
+                        fresh_oranges -= 1          
+            minutes += 1
 
-            return minutes if fresh_oranges == 0 else -1
-        
-
-
-    def bfs(self, q: Deque[Tuple[int, int]], grid: List[List[int]]) -> Deque[Tuple[int, int]]:
-        new_rotten = deque([])
-        while q:
-            r, c = q.popleft()
-
-            for dr, dc in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
-                x, y = r + dr, c + dc
-
-                if x < 0 or y < 0 or x >= len(grid) or y >= len(grid[0]):
-                    continue
-                elif grid[x][y] == 1:
-                    grid[x][y] = 2
-                    new_rotten.append((x, y))
-        
-        return new_rotten
+        return minutes if fresh_oranges == 0 else -1        
 
