4018b9df0907922c6364fd18fc8f386e6a4abb1c
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-16 23:24:55 +0000

    [LeetCode Sync] Runtime - 40 ms (60.22%), Memory - 16.7 MB (55.42%)

--- my-folder/0046-permutations/solution.py
+++ my-folder/0046-permutations/solution.py
@@ -1,16 +1,15 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        return self.permute_h(nums, [], [])
-    
-    def permute_h(self, nums: List[int], permutation: List[int], permutations: List[List[int]]) -> List[List[int]]:
-        if len(nums) == 0:
-            permutations.append(permutation)
+
+        def rec(nums, permutation: List[int], permutations: List[List[int]]) -> List[List[int]]:
+            if len(nums) == 0:
+                return permutations.append(permutation)
+                
+
+            for i in range(len(permutation)):
+                rec(nums[1:], permutation[:i] + [nums[0]] + permutation[i:], permutations)
+            rec(nums[1:], permutation + [nums[0]], permutations)
+
             return permutations
-
-        for i in range(len(permutation)):
-            _ = self.permute_h(nums[1:], permutation[:i] + [nums[0]] + permutation[i:], permutations)
-        _ = self.permute_h(nums[1:], permutation + [nums[0]], permutations)
-        return permutations
-
-
         
+        return rec(nums, [], [])
