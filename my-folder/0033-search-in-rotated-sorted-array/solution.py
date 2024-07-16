dab994b287aa5d9987a92a37cf19a3f444e98d9a
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-16 22:13:45 +0000

    [LeetCode Sync] Runtime - 38 ms (87.17%), Memory - 16.8 MB (70.25%)

--- my-folder/0033-search-in-rotated-sorted-array/solution.py
+++ my-folder/0033-search-in-rotated-sorted-array/solution.py
@@ -5,21 +5,13 @@ class Solution:
         while start <= end:
             mid = (start + end) // 2
 
-            if nums[mid] == target:
-                return mid
-            
-            if nums[mid] >= nums[start]:
-                if nums[start] <= target < nums[mid]:
-                    end = mid - 1
-                else:
-                    start = mid + 1
-            else:
-                if nums[mid] < target <= nums[end]:
-                    start = mid + 1
-                else:
-                    end = mid - 1
-                
-        
-        return -1
+            if nums[mid] == target : return mid
 
-        
+            if nums[start] <= target < nums[mid]:
+                end = mid - 1
+            elif nums[mid] > nums[end] or nums[mid] < target <= nums[end]:
+                start = mid + 1
+            else:
+                end = mid - 1
+
+        return -1
