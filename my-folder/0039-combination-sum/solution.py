849c80884cb761d9075373bdf5f871566f8e56d6
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-16 22:41:29 +0000

    [LeetCode Sync] Runtime - 67 ms (16.42%), Memory - 16.8 MB (10.35%)

--- my-folder/0039-combination-sum/solution.py
+++ my-folder/0039-combination-sum/solution.py
@@ -1,20 +1,20 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        def backtrack(candidates: List[int], target: int, combination: List[int], \
-                      combinations: List[List[int]]) -> List[List[int]]:
+        def c_sum(candidates: List[int], target: int, 
+                  combination: List[int] = [], combinations: List[int] = []) -> List[List[int]]:
             
-            if target < 0 or not candidates:
+            if not candidates or target < 0: 
                 return combinations
             elif target == 0:
                 return combinations.append(combination)
-            else:
-                ## To ensure unique combinations: candidate list is shrunk
-                # choose first and continue with given candidate list
-                v = candidates[0]
-                _ = backtrack(candidates, target - v, [v] + combination, combinations)
-
-                # dont choose first and discard first from candidate list 
-                _ = backtrack(candidates[1:], target, [] + combination, combinations)
-                return combinations
             
-        return backtrack(candidates, target, [], [])
+            c = candidates[0]
+            # choose candidate
+            c_sum(candidates[:], target - c, combination[:] + [c], combinations)
+            
+            # discard candidate
+            c_sum(candidates[1:], target, combination[:], combinations)
+            
+            return combinations
+        
+        return c_sum(candidates, target)
