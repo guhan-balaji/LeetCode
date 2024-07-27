0235faf4fda2875fa3930476ad6cd4f40063232f
guhan-balaji <65337911+guhan-balaji@users.noreply.github.com>
2024-07-27 23:28:34 +0000

    [LeetCode Sync] Runtime - 830 ms (43.84%), Memory - 32.3 MB (16.88%)

--- my-folder/0322-coin-change/solution.py
+++ my-folder/0322-coin-change/solution.py
@@ -1,17 +1,18 @@
-from typing import Dict
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
-        res = self.dfs(coins, amount, dict())
-        return res if res != float("inf") else -1
+        @cache
+        def cc(amount: int) -> int:
+            if amount in coins:
+                return 1
+            elif amount == 0:
+                return 0
+            elif amount < 0:
+                return float("inf")
+            else:
+                return min([1 + cc(amount - coin) for coin in coins])
+                # print(min_coins)
+                # return min_coins if min_coins != float("inf") else -1
 
-    def dfs(self, coins: List[int], amount: int, cache: Dict[int, int]):
-        if amount in cache:
-            return cache[amount]
-        elif amount == 0:
-            return 0
-        elif amount < 0:
-            return float("inf")
-        else:
-            min_coins = float("inf")
-            cache[amount] = min([1 + self.dfs(coins, amount - coin, cache) for coin in coins])
-            return cache[amount]
+        return cc(amount) if cc(amount) != float("inf") else -1
+            
+
